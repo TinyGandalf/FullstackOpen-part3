55b154758c32068df743be134dd14b3f1990bcbf
Giuliano Rosso <rossogiuliano@gmail.com>
2022-09-12 19:16:59 -0300

    Added CORS

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const express = require('express')
+const cors = require('cors')
 const morgan = require('morgan')
+const express = require('express')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 app.use(morgan(function (tokens, req, res) {
