a90d7caedd59240cb51d996713d912e2c22be191
Giuliano Rosso <rossogiuliano@gmail.com>
2022-09-12 22:44:17 -0300

    Exercise 3.17

--- index.js
+++ index.js
@@ -38,15 +38,12 @@ app.get('/api/persons', (request, response) => {
 //   response.send(person)
 // })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then(() => {
       response.status(204).end()
     })
-    .catch(error => {
-      console.error(error)
-      response.status(400).json({ error })
-    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -72,6 +69,21 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.send(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 // app.get('/info', (request, response) => {
 //   response.send(`<p>Phonebook has info for ${persons.length} people</p>
 //                  <p>${Date()}</p>`)
@@ -80,8 +92,18 @@ app.post('/api/persons', (request, response) => {
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
+const errorHandler = (error, request, response, next) => {
+  console.error(error)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'invalid ID' })
+  }
+
+  next(error)
+}
 
 app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
